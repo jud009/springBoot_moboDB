ee5e4abfda72aaca544176684bb01d83d23b17b4
Judiclei Almeida <judiclei41@gmail.com>
2020-04-16 16:40:38 -0300

    getting post by id

---
+++ src/main/java/com/example/demo/resources/PostResource.java
@@ -0,0 +1,28 @@
+package com.example.demo.resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Post;
+import com.example.demo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService postService;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Optional<Post> posts = postService.findById(id);
+		return ResponseEntity.ok().body(posts.get());
+	}
+
+}
---
+++ src/main/java/com/example/demo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.example.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Post;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Optional<Post> findById(String id) {
+		Optional<Post> postId = postRepository.findById(id);
+
+		if (postId.isEmpty()) {
+			throw new ObjectNotFoundException("Object not found");
+		}
+		return postId;
+	}
+
+}
