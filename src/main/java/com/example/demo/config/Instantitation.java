b1f2b52e97f7621d0cde243dced8ad1bc4cb2c20
Judiclei Almeida <judiclei41@gmail.com>
2020-04-16 17:10:42 -0300

    adding posts comments

--- src/main/java/com/example/demo/config/Instantitation.java
+++ src/main/java/com/example/demo/config/Instantitation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
 import com.example.demo.dto.AuthorDTO;
+import com.example.demo.dto.CommentDTO;
 import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.UserRepository;
 
@@ -39,12 +40,22 @@ public class Instantitation implements CommandLineRunner {
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
 				new AuthorDTO(maria));
-		
+
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		CommentDTO commetPost = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO commetPost1 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+
+		CommentDTO commetPost2 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(commetPost, commetPost1));
+		post2.getComments().addAll(Arrays.asList(commetPost2));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 		maria.getAllPosts().addAll(Arrays.asList(post1, post2));
-		userRepository.save(maria); 
+		userRepository.save(maria);
+
 	}
 
 }
