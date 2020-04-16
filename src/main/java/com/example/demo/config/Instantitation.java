49d1e183612039f44859c7af97a0db11a923122b
Judiclei Almeida <judiclei41@gmail.com>
2020-04-16 16:08:35 -0300

    referencing users posts

--- src/main/java/com/example/demo/config/Instantitation.java
+++ src/main/java/com/example/demo/config/Instantitation.java
@@ -15,32 +15,36 @@ import com.example.demo.repository.PostRepository;
 import com.example.demo.repository.UserRepository;
 
 @Configuration
-public class Instantitation  implements CommandLineRunner{
-	
+public class Instantitation implements CommandLineRunner {
+
 	@Autowired
-	private UserRepository userRepository; 
-	
+	private UserRepository userRepository;
+
 	@Autowired
-	private PostRepository postRepository; 
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-		
+
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		userRepository.saveAll(Arrays.asList(maria, alex, bob)); 
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria)); 
-		postRepository.saveAll(Arrays.asList(post1, post2)); 
-	
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		maria.getAllPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria); 
 	}
 
 }
